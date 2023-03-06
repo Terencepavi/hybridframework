5782e94e7651fc47061bb0b65d63684e43e268ff
Terencepavi <pavithrabalanae@gmail.com>
2023-03-06 09:30:56 +0530

    updated

--- tests/test_login.py
+++ tests/test_login.py
@@ -9,8 +9,9 @@ class TestLoginUI:
 
     @pytest.fixture(scope="function", autouse=True)
     def setup(self):
-        serv_driver = Service(executable_path=r"C:\Users\JiDi\Downloads\chromedriver_win32 (5)\chromedriver.exe")
-        self.driver = webdriver.Chrome(service=serv_driver)
+        # serv_driver = Service(executable_path=r"C:\Users\JiDi\Downloads\chromedriver_win32 (5)\chromedriver.exe")
+        # self.driver = webdriver.Chrome(service=serv_driver)
+        self.driver = webdriver.Chrome()
         self.driver.maximize_window()
         self.driver.implicitly_wait(20)
         self.driver.get("https://opensource-demo.orangehrmlive.com/")
