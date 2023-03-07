bbbcd174b8d1e9a6519a24e76738c6fd02adee35
Terencepavi <pavithrabalanae@gmail.com>
2023-03-07 09:36:55 +0530

    inherit concepts

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,27 +1,39 @@
+import time
+
 import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from assertpy import assert_that
+from selenium import webdriver
 from selenium.webdriver.common.by import By
+
+from base.webdriver_listner import WebDriverWrapper
+
+
+class TestLogin(WebDriverWrapper):
+    def test_valid_login(self):
+        self.driver.find_element(By.NAME, "username").send_keys("Admin")
+        self.driver.find_element(By.NAME, "password").send_keys("admin123")
+        self.driver.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
+        actual_text = self.driver.find_element(By.XPATH, "//h6[normalize-space()='Dashboard']").text
+        assert_that("Dashboard").is_equal_to(actual_text)
 
 
 class TestLoginUI:
-
-    @pytest.fixture(scope="function", autouse=True)
+    @pytest.fixture(scope='function')
     def setup(self):
-        # serv_driver = Service(executable_path=r"C:\Users\JiDi\Downloads\chromedriver_win32 (5)\chromedriver.exe")
-        # self.driver = webdriver.Chrome(service=serv_driver)
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
-        self.driver.implicitly_wait(20)
-        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        self.driver.implicitly_wait(10)
+        self.driver.get('https://opensource-demo.orangehrmlive.com/')
+
         yield
+        time.sleep(3)
         self.driver.quit()
 
-    def test_title(self):
+    def test_title(self, setup):
         actual_title = self.driver.title
-        assert_that("OrangeHRM").is_equal_to(actual_title)
+        assert actual_title == 'OrangeHRM'
+        assert_that('OrangeHRM').is_equal_to(actual_title)
 
-    def test_header(self):
-        actual_header = self.driver.find_element(By.XPATH, "//h5").text
-        assert_that("Login").is_equal_to(actual_header)
+    def test_header(self, setup):
+        actual_header = self.driver.find_element(By.XPATH, '//h5').text
+        assert_that(actual_header).is_equal_to('Login')
