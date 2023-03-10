96f6d5d1cee73c0cc8c0c4682bb3c1ade43a4bb2
Terencepavi <pavithrabalanae@gmail.com>
2023-03-10 09:34:26 +0530

    Data driven basics

--- utilities/data_source.py
+++ utilities/data_source.py
@@ -1,3 +1,4 @@
+from utilities import read_utils
 test_invalid_login_data = [
     ("Admin1", "admin1", "Invalid credentials"),
     ("Admin2", "admin2", "Invalid credentials")
@@ -11,3 +12,5 @@ test_add_valid_employee_data = [
     ["Admin", "admin123", "John", "J", "Wick", "John Wick", "John"],
     ["Admin", "admin123", "Peter", "p", "Wick", "Peter Wick", "Peter"]
 ]
+
+test_invalid_login_data=read_utils.get_csv_as_list("../test_data/test_invalid_login_data.csv")
